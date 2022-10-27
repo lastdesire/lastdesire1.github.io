b112b286e39aa197aceec626ee6d5cb48a4aa0df
lastdesire <deadinsidezz123450@mail.ru>
2022-10-27 15:12:20 +0300

    Update styles

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -6,14 +6,14 @@ import Photo from "../photo.png";
 const About = () => {
     return (
         <div className="about_me_text" color="white">
-            <img src={Photo} className="photo_style" alt="fireSpot"/>
-            <span> • About me: Gagin Artur{"\n"}</span>
-            <span> • 19 years old, studying at SPbU, SE.{"\n"}</span>
-            <span> • I've been studying programming since I was 16.{"\n"}</span>
-            <span> • During this time, I managed to work with the{"\n"}</span>
-            <span> following programming languages and libraries:{"\n"}</span>
-            <span> • C, C++, C#, Delphi, Python, OCaml, JS (React).{"\n"}</span>
-            <span> • Now I am studying OCaml and the ReactJS library more deeply. </span>
+            <img className="photo_style" alt="fireSpot" src={Photo}/>
+            <div>{"\n"} • About me: Gagin Artur{"\n"}</div>
+            <div> • 19 years old, studying at SPbU, SE.{"\n"}</div>
+            <div> • I've been studying programming since I was 16.{"\n"}</div>
+            <div> • During this time, I managed to work with the{"\n"}</div>
+            <div> following programming languages and libraries:{"\n"}</div>
+            <div> • C, C++, C#, Delphi, Python, OCaml, JS (React).{"\n"}</div>
+            <div> • Now I am studying OCaml and the ReactJS library more deeply. </div>
         </div>
 
     );
